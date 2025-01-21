38dcfceffae1ba461186d06fdae811e59431a8b6
kevensouzz <skilltwins2015@gmail.com>
2025-01-21 15:01:37 -0300

    responsive auth pages

--- client/src/components/header.php
+++ client/src/components/header.php
@@ -26,53 +26,65 @@ $currentPath = getCleanPath($_SERVER['REQUEST_URI']);
       <line x1="19" x2="5" y1="18" y2="18" />
     </svg>
   </button>
-
-  <nav id="mobile-navbar">
-    <header id="mobile-navbar-header">
-      <?php
-      include('banner.php')
-      ?>
-
-      <button id="btn-close-mobile-navbar">
-        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#0000c8"
-          stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x">
-          <path d="M18 6 6 18" />
-          <path d="m6 6 12 12" />
-        </svg>
-      </button>
-    </header>
-
-    <span id="mobile-navbar-content">
-      <a href="/home" class="<?= $currentPath == '/home' ? 'active' : '' ?>">Home</a>
-      <a href="/politics" class="<?= $currentPath == '/politics' ? 'active' : '' ?>">Politics</a>
-      <a href="/auth" class="<?= $currentPath == '/auth/register' || $currentPath == '/auth/login' || $currentPath == '/auth/reset' ? 'active' : '' ?>">Auth</a>
-    </span>
-  </nav>
-
-  <script>
-    const btnOpenMobileNavbar = document.getElementById('btn-open-mobile-navbar');
-    const btnCloseMobileNavbar = document.getElementById('btn-close-mobile-navbar');
-    const mobileNavbar = document.getElementById('mobile-navbar');
-    const headerBanner = document.querySelector('#header .banner');
-
-    btnOpenMobileNavbar.addEventListener('click', function() {
-      if (mobileNavbar.style.display === 'none' || mobileNavbar.style.display === '') {
-        mobileNavbar.style.display = 'flex';
-        btnOpenMobileNavbar.style.display = 'none';
-        headerBanner.style.display = 'none';
-      }
-    });
-    
-    btnCloseMobileNavbar.addEventListener('click', function() {
-      if (mobileNavbar.style.display === 'flex') {
-        btnOpenMobileNavbar.style.display = 'flex';
-        headerBanner.style.display = 'block';
-        mobileNavbar.style.display = 'none';
-      }
-    })
-  </script>
 </header>
 
 <span class="header-division-line">
   <span></span>
 </span>
+
+<nav id="mobile-navbar">
+  <header id="mobile-navbar-header">
+    <?php
+    include('banner.php')
+    ?>
+
+    <button id="btn-close-mobile-navbar">
+      <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#0000c8"
+        stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x">
+        <path d="M18 6 6 18" />
+        <path d="m6 6 12 12" />
+      </svg>
+    </button>
+  </header>
+
+  <span class="mobile-header-division-line">
+    <span></span>
+  </span>
+
+  <main>
+    <span id="mobile-navbar-content">
+      <a href="/home" class="<?= $currentPath == '/home' ? 'active' : '' ?>">Home</a>
+      <a href="/politics" class="<?= $currentPath == '/politics' ? 'active' : '' ?>">Politics</a>
+      <a href="/auth" class="<?= $currentPath == '/auth/register' || $currentPath == '/auth/login' || $currentPath == '/auth/reset' ? 'active' : '' ?>">Auth</a>
+    </span>
+  </main>
+</nav>
+
+<script>
+  const btnOpenMobileNavbar = document.getElementById('btn-open-mobile-navbar');
+  const btnCloseMobileNavbar = document.getElementById('btn-close-mobile-navbar');
+  const mobileNavbar = document.getElementById('mobile-navbar');
+  const headerBanner = document.querySelector('#header .banner');
+  const headerDivisionLine = document.querySelector('.header-division-line');
+  const body = document.body;
+
+  btnOpenMobileNavbar.addEventListener('click', function() {
+    if (mobileNavbar.style.display === 'none' || mobileNavbar.style.display === '') {
+      mobileNavbar.style.display = 'flex';
+      body.classList.add('no-scroll');
+      btnOpenMobileNavbar.style.display = 'none';
+      headerBanner.style.display = 'none';
+      headerDivisionLine.style.display = 'none';
+    }
+  });
+
+  btnCloseMobileNavbar.addEventListener('click', function() {
+    if (mobileNavbar.style.display === 'flex') {
+      btnOpenMobileNavbar.style.display = 'flex';
+      headerDivisionLine.style.display = 'flex';
+      headerBanner.style.display = 'block';
+      body.classList.remove('no-scroll');
+      mobileNavbar.style.display = 'none';
+    }
+  })
+</script>
